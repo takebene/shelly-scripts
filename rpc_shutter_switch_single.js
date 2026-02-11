b5604d9075026ab9349f322a7b6f3b5fa50b1952
Linke, Benedikt <benedikt.linke@4soft.de>
2026-02-11 18:44:45 +0100

    fix: update description for RPC Shutter Switch in manifest.json and add rpc_shutter_switch_single.js

---
+++ rpc_shutter_switch_single.js
@@ -0,0 +1,92 @@
+let CONFIG = {
+  ip: "192.XXX.XXX.XXX",
+};
+
+let RemoteShelly = {
+  _cb: function (result, error_code, error_message, callback) {
+    let rpcResult = JSON.parse(result.body);
+    let rpcCode = result.code;
+    let rpcMessage = result.message;
+    callback(rpcResult, rpcCode, rpcMessage);
+  },
+  composeEndpoint: function (method) {
+    return "http://" + this.address + "/rpc/" + method;
+  },
+  call: function (rpc, data, callback) {
+    let postData = {
+      url: this.composeEndpoint(rpc),
+      body: data,
+    };
+    Shelly.call("HTTP.POST", postData, RemoteShelly._cb, callback);
+  },
+  getInstance: function (address) {
+    let rs = Object.create(this);
+    // remove static method
+    rs.getInstance = null;
+    rs.address = address;
+    return rs;
+  },
+};
+
+let remoteShelly = RemoteShelly.getInstance(CONFIG.ip);
+
+Shelly.addEventHandler(function (statusEvent) {
+  print("Status Event: " + JSON.stringify(statusEvent));
+
+  let result = statusEvent.info;
+
+  // Input 0 = open, Input 1 = close
+  let action = result.component === "input:0" ? "open" : "close";
+
+  // Only handle input:0 and input:1
+  if (result.component !== "input:0" && result.component !== "input:1") {
+    return;
+  }
+
+  if (result.event === "single_push") {
+    remoteShelly.call(
+      "Cover.GetStatus",
+      { id: 0 },
+      doExcecute(action, remoteShelly),
+    );
+  } else if (result.event === "long_push") {
+    //remoteShelly.call('Cover.GoToPosition', { id: 0, pos: 50 }, nullCallback());
+    print("long_push");
+  }
+});
+
+function doExcecute(action, targetShelly) {
+  return function (result, error_code, error_message, ud) {
+    console.log("Current action: " + action);
+    console.log("Current targetShelly: " + targetShelly);
+    console.log(
+      "Cover status result: " + JSON.stringify(result),
+      error_code,
+      error_message,
+    );
+
+    if (result.state === "opening" || result.state === "closing") {
+      targetShelly.call("Cover.Stop", { id: 0 }, nullCallback());
+    } else if (result.state === "stopped") {
+      if (action === "close") {
+        targetShelly.call("Cover.Close", { id: 0 }, nullCallback());
+      } else if (action === "open") {
+        targetShelly.call("Cover.Open", { id: 0 }, nullCallback());
+      } else {
+        targetShelly.call("Cover.Stop", { id: 0 }, nullCallback());
+      }
+    } else {
+      if (action === "open" && result.state !== "open") {
+        targetShelly.call("Cover.Open", { id: 0 }, nullCallback());
+      } else if (action === "close" && result.state !== "closed") {
+        targetShelly.call("Cover.Close", { id: 0 }, nullCallback());
+      } else {
+        targetShelly.call("Cover.Stop", { id: 0 }, nullCallback());
+      }
+    }
+  };
+}
+
+function nullCallback() {
+  return function () {};
+}
